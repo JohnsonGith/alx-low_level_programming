6ce7c2dfe87348b87562971bcbceeaaf3aecd9b0
Johnson Omotunde <johnsonwebsax@gmail.com>
2022-07-02 02:17:02 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
- * Return: ALways 0 (Success)
+ * main - prints the alphabet in lowerecase,
+ * followed by a new line
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int n, m;
+	char ch;
 
-	for (n = 48; n <= 56; n++)
+	for (ch = 97; ch <= 122; ch++)
 	{
-		for (m = 49; m <= 57; m++)
-		{
-			if (m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+		putchar(ch);
 	}
-	putchar('\n');
+	putchar(10);
+
 	return (0);
-}
